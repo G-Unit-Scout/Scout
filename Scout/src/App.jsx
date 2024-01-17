77d0ffdb73c6593270e86053ffa94ab9f3651a61
WRhee123 <rhee.william@yahoo.com>
2024-01-17 15:44:06 -0500

    added tailwind,daisyUI, and react-beautiful-dnd

---
+++ Scout/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
