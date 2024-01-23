456d3da304d432e626ee26dc8d1099e1cd2c4166
WRhee123 <rhee.william@yahoo.com>
2024-01-23 14:50:56 -0500

    added email and pw inputs/ galvanize colors

--- Scout/src/components/AdminLogin.jsx
+++ Scout/src/components/AdminLogin.jsx
@@ -7,17 +7,36 @@ const [login, setlogin] = useState(false)
 return (
     <>
       <div className='flex flex-col justify-center items-center h-screen w-absolute bg-white'>
-        <div className='flex flex-col justify-center items-center h-screen absolute bg-blue-400'>
-          <input
-            className='placeholder-gray-500 border border-gray-300 p-2 mb-2'
-            type='text'
-            placeholder='Enter your first name'
-          />
-          <input
-            className='placeholder-gray-500 border border-gray-300 p-2'
-            type='text'
-            placeholder='Enter your last name'
-          />
+        <div className='flex flex-col items-center h-[800px] w-[600px] absolute bg-blue-400 rounded-xl'>
+            <div className='flex flex-col justify-center items-center bg-[rgba(239,110,71,255)] h-[100px] w-[500px]'>
+                <span className='text-[40px] font-bold text-white'>Login</span>
+            </div>
+
+            <div className='flex flex-col justify-center items-center h-[300px] w-[600px] bg-[rgba(13,15,74,255)] mt-[100px]'>
+              
+              <div className='flex flex-col bg-red-400 h-[100px] w-[500px]'>
+                <span className='text-white'>Email</span>
+                <input
+                id='email'
+                className='placeholder-gray-500 border border-gray-300 p-2 mb-2 rounded-xl'
+                type='text'
+                placeholder='Enter email'
+                />
+                </div>
+
+                <div className='flex flex-col bg-yellow-400 h-[100px] w-[500px]'>
+                    <span className='text-white'>Password</span>
+                    <input
+                    id='password'
+                    className='placeholder-gray-500 border border-gray-300 p-2 rounded-xl'
+                     type='text'
+                    placeholder='Enter password'
+                    />
+                </div>  
+                
+            </div>
+          
+          
         </div>
       </div>
     </>
