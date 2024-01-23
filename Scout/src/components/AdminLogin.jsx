b62c47a31a422e34e1af409df5de918b888f7819
WRhee123 <rhee.william@yahoo.com>
2024-01-23 14:03:22 -0500

    created component

---
+++ Scout/src/components/AdminLogin.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+const AdminLogin = () => {
+
+const [login, setlogin] = useState(false)
+
+    return (
+        <>
+        
+        
+        
+        
+        </>
+    )
+}
+
+export default AdminLogin
