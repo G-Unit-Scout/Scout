df51a3420c3eccdd168879a3979bda1795e2a268
WRhee123 <rhee.william@yahoo.com>
2024-01-23 14:14:08 -0500

    added css

--- Scout/src/components/AdminLogin.jsx
+++ Scout/src/components/AdminLogin.jsx
@@ -4,14 +4,25 @@ const AdminLogin = () => {
 
 const [login, setlogin] = useState(false)
 
-    return (
-        <>
-        
-        
-        
-        
-        </>
-    )
+return (
+    <>
+      <div className='flex flex-col justify-center items-center h-screen w-absolute bg-white'>
+        <div className='flex flex-col justify-center items-center h-screen absolute bg-blue-400'>
+          <input
+            className='placeholder-gray-500 border border-gray-300 p-2 mb-2'
+            type='text'
+            placeholder='Enter your first name'
+          />
+          <input
+            className='placeholder-gray-500 border border-gray-300 p-2'
+            type='text'
+            placeholder='Enter your last name'
+          />
+        </div>
+      </div>
+    </>
+  );
+  
 }
 
 export default AdminLogin
