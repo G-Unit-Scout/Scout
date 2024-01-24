9f303b13a158411b63ce3b61b85c083021fd8d7e
kiel buchergallimore <kielbucher@gmail.com>
2024-01-24 11:23:15 -0600

    initital commit

--- Scout/src/App.jsx
+++ Scout/src/App.jsx
@@ -2,14 +2,18 @@ import { useState } from 'react'
 import { useEffect } from 'react'
 import './App.css'
 import AdminLogin from './components/AdminLogin'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <NavBar />
      <button className='btn btn-primary'>It Worked!</button>
      {/* <AdminLogin/> */}
+    <Footer />
     </>
   )
 }
---
+++ Scout/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+
+
+function Footer() {
+
+    return (
+    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+        <aside>
+            <p>Copyright © 2024 - All right reserved by Galvanize Inc Ltd</p>
+        </aside>
+    </footer>
+    )
+}
+
+export default Footer;
