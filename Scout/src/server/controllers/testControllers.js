d634008039cfab4888d9d16c215d646fa0f44eaa
Alex Fields <134031883+Zyvod@users.noreply.github.com>
2024-01-24 17:44:43 -0600

    Merged login auth server and kanban servers

---
+++ Scout/src/server/controllers/testControllers.js
@@ -0,0 +1,95 @@
+import db from '../../database/db.js';
+
+const testControllers = {
+  fetchPartnerJobs: async (req, res) => {
+    const partnerJobQuery = `SELECT * FROM partner_jobs;`
+    try {
+      const results = await db.query(partnerJobQuery)
+      res.status(200).send(results.rows)
+    } catch(error) {
+      console.error(`Error fetching jobs ${error}`)
+      res.status(500).send('Error, could not fetch jobs list')
+    }
+  },
+
+  fetchUsers: async (req,res) => {
+    const usersQuery = `SELECT * FROM users;`
+    try {
+      const results = await db.query(usersQuery)
+      res.status(200).send(results.rows)
+    } catch(error) {
+      console.error(`Error fetching users ${error}`)
+      res.status(500).send(`Error, could not fetch users`)
+    }
+  },
+
+  fetchAnnouncements: async (req,res) => {
+    const announcementQuery = `SELECT * FROM announcements;`
+    try  {
+      const results = await db.query(announcementQuery)
+      res.status(200).send(results.rows)
+    } catch(error) {
+      console.error(`Error fetching announcements ${error}`)
+      res.status(500).send(`Error, could not fetch announcements`)
+    }
+  },
+
+  fetchNotifications: async (req,res) => {
+    const userID = req.params.id
+    const notificationQuery = `SELECT * FROM notifications WHERE created_for = $1 AND read = false;`
+    try  {
+      console.log(`Joo did shtuff`)
+      const results = await db.query(notificationQuery,[userID])
+      res.status(200).send(results.rows)
+    } catch(error) {
+      console.error(`Error fetching notifications ${error}`)
+      res.status(500).send(`Error, could not fetch notifications`)
+    }
+  },
+
+  fetchStudentKanban: async (req,res) => {
+    const userID = req.params.id
+    const kanbanQuery = ` SELECT
+     job_status.*,
+     partner_jobs.*,
+     user_notes.*,
+     users.user_name
+     FROM
+     job_status
+     JOIN users ON job_status.user_id = users.user_id
+     JOIN partner_jobs ON job_status.job_id = partner_jobs.job_id
+     JOIN user_notes ON job_status.note_id = user_notes.note_id
+     WHERE users.user_id = $1;`
+     try {
+      const results = await db.query(kanbanQuery,[userID])
+      res.status(200).send(results.rows)
+     } catch(error) {
+      console.error(`Error fetching kanban ${error}`)
+      res.status(500).send(`Error, couldn't fetch student kanban`)
+     }
+  },
+
+  fetchCohortKanban: async (req,res) => {
+    const cohortID = req.params.id
+    const cohortKanbanQuery =   `SELECT
+    job_status.*,
+    partner_jobs.*,
+    user_notes.*,
+    users.user_name
+    FROM job_status
+    JOIN cohorts ON job_status.cohort_id = cohorts.cohort_id
+    JOIN partner_jobs ON job_status.job_id = partner_jobs.job_id
+    JOIN user_notes ON job_status.note_id = user_notes.note_id
+    JOIN users ON job_status.user_id = users.user_id
+    WHERE cohorts.cohort_id = $1;`
+    try {
+      const results = await db.query(cohortKanbanQuery,[cohortID])
+      res.status(200).send(results.rows)
+    } catch(error) {
+      console.error(`Error fetching cohort kanban ${error}`)
+      res.status(500).send(`Error, could not fetch cohort kanban`)
+    }
+  }
+};
+
+export default testControllers
