6c8bb7991fe2b85780f857eaeb1cb54dc8f9b613
Michael Mote <michael.mote1993@gmail.com>
2024-01-24 14:42:44 -0600

    Add axios dependency and update AdminLogin component

--- Scout/src/components/AdminLogin.jsx
+++ Scout/src/components/AdminLogin.jsx
@@ -27,6 +27,15 @@ const AdminLogin = () => {
                         <div className='flex flex-col justify-center items-center bg-[rgba(239,110,71,255)] h-[120px] w-[600px] rounded-t-xl'>
                             <span className='text-[40px] font-bold text-white'>Login</span>
                         </div>
+    return (
+            <>
+                <div className='flex flex-col justify-center items-center h-screen w-absolute bg-white'>
+            
+                    <div className='flex flex-col items-center h-[530px] w-[600px] absolute bg-[rgba(13,15,74,255)] rounded-xl'>
+                
+                        <div className='flex flex-col justify-center items-center bg-[rgba(239,110,71,255)] h-[120px] w-[600px] rounded-t-xl'>
+                            <span className='text-[40px] font-bold text-white'>Login</span>
+                        </div>
 
                         <div className='flex flex-col justify-center items-center h-[300px] w-[600px] bg-[rgba(13,15,74,255)] mt-[40px]'>
                 
@@ -60,8 +69,30 @@ const AdminLogin = () => {
                                 </button>
                             </div>
                         </div>
+                            <div className='flex flex-col bg-[rgba(13,15,74,255)] h-[100px] w-[500px]'>
+                                <span className='text-white'>Password</span>
+                                <input
+                                    id='password'
+                                    value={password}
+                                    onChange={e => setPassword(e.target.value)}
+                                    className='placeholder-gray-500 border border-gray-300 p-2 rounded-xl mt-[3px] bg-[rgba(22,26,40,255)]'
+                                    type='text'
+                                    placeholder='Enter password'
+                                />
+                            </div>
+
+                            <div className='mt-3'>
+                                <button onClick={handleLogin} className='h-[50px] w-[500px] bg-[rgba(239,110,71,255)] rounded-xl'>
+                                    <span className='text-white '>Sign In</span>
+                                </button>
+                            </div>
+                        </div>
             
           
+                    </div>
+                </div>
+            </>
+    );
                     </div>
                 </div>
             </>
