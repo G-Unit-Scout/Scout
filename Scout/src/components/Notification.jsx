a5b3475603099950debb898a1a4f3680039541cb
kiel buchergallimore <kielbucher@gmail.com>
2024-01-31 14:12:15 -0600

    read works

--- Scout/src/components/Notification.jsx
+++ Scout/src/components/Notification.jsx
@@ -1,12 +1,27 @@
 
 const Notification = ({ userId, notification }) => {
-    
+
+    const readNotification = async () => {
+        const res = await fetch(`https://scouttestserver.onrender.com/api/updatenotifications/${notification.id}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({ 
+            read: true 
+            })
+        });
+    }
+
+    const handleClick = (e) => {
+        readNotification();
+    }
 
     return (
     <>
     <li className="flex flex-row justify-center space-between items-center">
         <a className="hover:bg-slate-100 w-48"> {notification.message}</a>
-        <input type="checkbox" className="checkbox"/>
+        <input type="checkbox" className="checkbox" onClick={handleClick} />
     </li>
     </>
     )
